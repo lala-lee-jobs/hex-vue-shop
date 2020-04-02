10fc0cf69f1de72922497d0bae985ed7df73988b
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-03 01:30:24 +0800

    製作登入介面

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
-    <button type="button" class="btn btn-success">Success</button>
-    <button type="button" class="btn btn-danger">Danger</button>
-    <button type="button" class="btn btn-warning">Warning</button>
-    <button type="button" class="btn btn-info">Info</button>
-    <button type="button" class="btn btn-light">Light</button>
-    <button type="button" class="btn btn-dark">Dark</button>
-    <a href="#">12345</a>
   </div>
 </template>
 
@@ -18,12 +8,6 @@
 export default {
   name: 'App',
   created() {
-    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
-    fetch(api)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-      });
   },
 }
 </script>
