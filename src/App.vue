e8dfc2a15ba7c62429131e2db706b0889db1c7b6
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-02 21:50:50 +0800

    引用 Bootstrap 套件，並客製化樣式

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <a href="#">12345</a>
   </div>
 </template>
 
@@ -20,5 +29,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "~bootstrap/scss/bootstrap";
+@import "./assets/all";
 </style>
