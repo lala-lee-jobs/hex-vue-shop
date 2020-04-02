73f1b45dd9cc1e80ff95749106a4be657850325a
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-03 02:42:46 +0800

    Vue 中運用 Bootstrap 及 jQuery

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="text-right mt-4">
-            <button class="btn btn-primary">建立新的產品</button>
+            <button class="btn btn-primary" @click="openModal">建立新的產品</button>
             <table class="table mt-4">
                 <thead>
                     <th width="120">分類</th>
@@ -31,10 +31,32 @@
                     </tr>
                 </tbody>
             </table>
+            <!-- Modal -->
+            <div class="modal fade" id="productModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
+import $ from 'jquery';
+
 export default {
     data() {
         return {
@@ -48,6 +70,9 @@ export default {
                 console.log('data', response.data);
                 this.products = response.data.products;
             });
+        },
+        openModal() {
+            $('#productModal').modal('show');
         }
     },
     created() {
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
- 
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import 'bootstrap'; 
 
-import App from './App'
-import router from './router'
+import App from './App';
+import router from './router';
 
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
