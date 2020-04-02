c93a902d7f668aded742911c602937d0f1f0b1b6
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-03 01:50:30 +0800

    驗證登入及 Vue Router 的配置

--- src/main.js
+++ src/main.js
@@ -18,4 +18,24 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
+
+router.beforeEach((to, from, next) => {
+  console.log('to', to , 'from', from, 'next', next);
+  if(to.meta.requiresAuth) {
+    console.log('這裡需要驗證');
+    const api = `${process.env.APIPATH}/api/user/check`;
+    axios.post(api).then((response) => {
+      console.log('data', response.data);
+      if(response.data.success) {
+          next();
+      } else {
+        next({
+          path: '/login'
+        });
+      }
+    });
+  } else {
+    next();
+  }
+});
--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,17 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      meta: { requiresAuth: true }
     },
     {
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '*',
+      redirect: 'login'
     }
   ]
 })
