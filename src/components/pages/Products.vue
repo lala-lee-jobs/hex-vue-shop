5a01c16fe965723b370d4a31850e11ec5382290c
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-03 10:49:20 +0800

    串接上傳檔案 API

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -57,17 +57,17 @@
                         <div class="form-group">
                         <label for="image">輸入圖片網址</label>
                         <input type="text" class="form-control" id="image"
-                            placeholder="請輸入圖片連結">
+                            placeholder="請輸入圖片連結"
+                            v-model="tempProduct.imageUrl">
                         </div>
                         <div class="form-group">
                         <label for="customFile">或 上傳圖片
                             <i class="fas fa-spinner fa-spin"></i>
                         </label>
                         <input type="file" id="customFile" class="form-control"
-                            ref="files">
+                            ref="files" @change="uploadFile">
                         </div>
-                        <img img="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=828346ed697837ce808cae68d3ddc3cf&auto=format&fit=crop&w=1350&q=80"
-                        class="img-fluid" alt="">
+                        <img :src="tempProduct.imageUrl" class="img-fluid" alt="">
                     </div>
                     <div class="col-sm-8">
                         <div class="form-group">
@@ -199,6 +199,24 @@ export default {
                 this.isNew = false;
             }
             $('#productModal').modal('show');
+        },
+        uploadFile() {
+            console.log(this);
+            const uploadedFile = this.$refs.files.files[0];
+            const formData = new FormData();
+            formData.append('file-to-upload', uploadedFile);
+            const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/upload`;
+            this.axios.post(url, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then(response => {
+                console.log(response.data);
+                if(response.data.success) {
+                    this.$set(this.tempProduct, 'imageUrl', response.data.imageUrl);
+                    console.log(this.tempProduct);
+                }
+            });
         },        
         getProducts() {
             const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
